9509a6b3213db69597194ee95c21022679ae36be
Andrea Covelli <andrea.covelli@gmail.com>
2023-01-09 23:44:44 +0100

    fix

--- app/Http/Resources/RosterResource.php
+++ app/Http/Resources/RosterResource.php
@@ -21,7 +21,6 @@ class RosterUserResource extends JsonResource
         $teaching = false;
 
         if ($course && $course !== $GUSTS_KEY) {
-            dd($course);
             $courseUser = $course->users->where('id', $this->id)->first();
             if ($courseUser) {
                 $in_charge = $courseUser->pivot->in_charge;
