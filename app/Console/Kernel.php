a4c93a0c2564ba080fd204b18c8b9da21912d17a
Andrea Covelli <andrea.covelli@gmail.com>
2023-02-04 08:18:37 +0100

    fix crone jobs

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -17,10 +17,20 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command(ResetASDMembership::class)->cron('0 58 23 31 AUG ? *')->runInBackground();;
+        //$schedule->command(ResetASDMembership::class)->cron('0 58 23 31 AUG ? *')->runInBackground();;
+        $schedule->command(ResetASDMembership::class)->yearlyOn(8, 31, '23:59')->runInBackground();;
         $schedule->command(EmptyTmpFolder::class)->dailyAt('23:59')->runInBackground();;
     }
 
+    /**
+     * Get the timezone that should be used by default for scheduled events.
+     *
+     * @return \DateTimeZone|string|null
+     */
+    protected function scheduleTimezone()
+    {
+        return 'Europe/Rome';
+    }
     /**
      * Register the commands for the application.
      *
