c65987c88bd744daf0214dbea5ffe7a4f9d91340
Andrea Covelli <andrea.covelli@gmail.com>
2023-03-07 16:25:36 +0100

    bho

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -29,7 +29,7 @@ class AuthController extends Controller
                 $user->isLogin = true;
                 return new LoginResource($user);
             } else {
-                $response = ['message' => 'Not Found'];
+                $response = ['message' => 'Not Found P'];
 
                 return response($response, 404);
             }
